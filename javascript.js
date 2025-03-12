18f08185106bff68c903366653c0ce659d25b45b
HunterHaller <huntinghollers@proton.me>
2025-03-12 16:27:24 +0100

    Changed project to have functional UI

--- javascript.js
+++ javascript.js
@@ -11,42 +11,43 @@ function getComputerChoice() {
     }
 }
 
-function getHumanChoice() {
-    return (prompt("CHOOSE YOUR WEAPON: "))
-}
-
 function playGame() {
     let humanScore = 0
     let computerScore = 0
+
+    let playerScoreHolder = document.querySelector("#playerScore")
+    let computerScoreHolder = document.querySelector("#computerScore")
+    let roundResults = document.querySelector("#roundResults")
+    let finalResults = document.querySelector("#results")
+
+    console.log("The game begins!")
 
     function playRound(humanChoice, computerChoice) {
         let humanFixed = humanChoice.toLowerCase()
         console.log("You chose " + humanFixed)
         console.log("The computer chose " + computerChoice)
+
+        let roundText = "You played "
+        roundResults.textContent = roundText.concat(" ", humanFixed, ", and the computer played ", computerChoice)
+
         if (humanFixed == computerChoice) {
-            console.log("Tied!!!!")
-        }
-        else if (humanFixed == "rock") {
+            console.log("Tied, no points given!!!!")
+        } else if (humanFixed == "rock") {
             if (computerChoice == "paper") {
                 computerScore++
-                console.log("Point computer!")
             }
             else if (computerChoice == "scissors") {
                 humanScore++
-                console.log("Point to you!")
             }
-        }
-        else if (humanFixed == "paper") {
+        } else if (humanFixed == "paper") {
             if (computerChoice == "scissors") {
                 computerScore++
-                console.log("Point computer!")
             }
             else if (computerChoice == "rock") {
                 humanScore++
                 console.log("Point to you!")
             }
-        }
-        else if (humanFixed == "scissors") {
+        } else if (humanFixed == "scissors") {
             if (computerChoice == "rock") {
                 computerScore++
                 console.log("Point computer!")
@@ -56,30 +57,33 @@ function playGame() {
                 console.log("Point to you!")
             }
         }
-        else {
-            return ("Bad input, try again!")
-        }
-    }
-    //Play 5 rounds:
-    playRound(getHumanChoice(), getComputerChoice())
-    playRound(getHumanChoice(), getComputerChoice())
-    playRound(getHumanChoice(), getComputerChoice())
-    playRound(getHumanChoice(), getComputerChoice())
-    playRound(getHumanChoice(), getComputerChoice())
 
-    //Declare the winner!
-    console.log("FINAL SCORES:")
-    console.log("COMPUTER: " + computerScore)
-    console.log("YOU: " + humanScore)
-    if (humanScore < computerScore){
-        return("Computer wins overall!")
+        playerScoreHolder.textContent = humanScore
+        computerScoreHolder.textContent = computerScore
     }
-    else if (humanScore > computerScore){
-        return("You win the whole game!")
-    }
-    else {
-        return("A TIE!?!?!?")
-    }
+
+    let list = document.querySelector(".buttonList");
+
+    list.addEventListener("click", (event) => {
+        let target = event.target;
+        playRound(target.id, getComputerChoice())
+
+        if ((humanScore == 5) || (computerScore == 5)) {
+            if (humanScore < computerScore) {
+                finalResults.textContent = "Computer wins overall!"
+                return ("Computer wins overall!")
+            }
+            else if (humanScore > computerScore) {
+                finalResults.textContent = "You win!!!"
+                return ("You win the whole game!")
+            }
+            else {
+                return ("A TIE!?!?!?")
+            }
+        }
+    })
+
+
 }
 
 console.log(playGame())
